57d17b0f83bfc233f07e4288379a22d3a844291a
Serhii Kotsar <sergejkotsar94@gmail.com>
2024-05-06 21:09:19 +0300

    added the first hw before git

--- lesson_01/homework_01.py
+++ lesson_01/homework_01.py
@@ -1,31 +1,31 @@
 # task 01 == Виправте синтаксичні помилки
-print("Hello", end = " ")
-    print("world!")
+print("Hello world!")
+
 # task 02 == Виправте синтаксичні помилки
 hello = "Hello"
 world = "world"
 if True:
-print(f"{hello} {world}!")
+    print(f"{hello} {world}!")
 
 # task 03  == Вcтавте пропущену змінну у ф-цію print
 for letter in "Hello world!":
-    print()
+    print(letter)
 
 # task 04 == Зробіть так, щоб кількість бананів була
 # завжди в чотири рази більша, ніж яблук
 apples = 2
-banana = x
+banana = apples * 4
 
 # task 05 == виправте назви змінних
-1_storona = 1
-?torona_2 = 2
-сторона_3 = 3
-$torona_4 = 4
+first_side = 1
+second_side = 2
+third_side = 3
+fourth_side = 4
 
 # task 06 == Порахуйте периметр фігури з task 05
 # та виведіть його для користувача
-perimetery = ? + ? + ? + ?
-print()
+perimeter = first_side + second_side + third_side + fourth_side
+print(f'Perimetr is: {perimeter}')
 
 
 """
@@ -39,6 +39,11 @@ print()
 У саду посадили 4 яблуні. Груш на 5 більше яблунь, а слив - на 2 менше.
 Скільки всього дерев посадили в саду?
 """
+apple_tree = 4
+pear_tree = apple_tree + 5
+plum_tree = apple_tree - 2
+sum_trees = apple_tree + pear_tree + plum_tree
+print(f'In common there are: {sum_trees} trees')
 
 # task 08
 """
@@ -46,6 +51,10 @@ print()
 Після обіду температура опустилася на 10 градусів.
 Надвечір потепліло на 4 градуси. Яка температура надвечір?
 """
+before_lunch = 5
+after_lunch = before_lunch - 10
+evening_time = after_lunch + 4
+print(f'There is {evening_time} degrees in the evening')
 
 # task 09
 """
@@ -54,9 +63,19 @@ print()
 Скількі сьогодні дітей у театральному гуртку?
 """
 
+boys = 24
+girls = boys / 2
+today_at_group = int((boys - 1) + (girls - 2))
+print(f'There are {today_at_group} children in a group today')
+
 # task 10
 """
 Перша книжка коштує 8 грн., друга - на 2 грн. дороже,
 а третя - як половина вартості першої та другої разом.
 Скільки будуть коштувати усі книги, якщо купити по одному примірнику?
 """
+first_book = 8
+second_book = first_book + 2
+third_book = (first_book + second_book) / 2
+sum_books = int(first_book + second_book + third_book)
+print(f'All books costs {sum_books} UAH')
