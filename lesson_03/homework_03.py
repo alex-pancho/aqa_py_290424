453edaeb4dfaa68d3b774915345c14df496454a6
Tetiana Suduk <kalatenkotetiana@gmail.com>
2024-05-09 11:16:56 +0300

    homework_03.py

--- lesson_03/homework_03.py
+++ lesson_03/homework_03.py
@@ -9,11 +9,6 @@ print(alice_in_wonderland)
 # task 01 == Розділіть змінну alice_in_wonderland так, щоб вона займала декілька фізичних лінії
 # task 02 == Знайдіть та екрануйте всі символи одинарної дужки у тексті
 # task 03 == Виведіть змінну alice_in_wonderland на друк
-=======
-alice_in_wonderland = '"Would you tell me, please, which way I ought to go from here?"\n"That depends a good deal on where you want to get to," said the Cat.\n"I don't much care where ——" said Alice.\n"Then it doesn't matter which way you go," said the Cat.\n"—— so long as I get somewhere," Alice added as an explanation.\n"Oh, you're sure to do that," said the Cat, "if you only walk long enough."'
-# task 01 == Розділіть змінну alice_in_wonderland так, щоб вона займала декілька фізичних лінії
-# task 02 == Знайдіть та екрануйте всі символи одинарної дужки у тексті
-# task 03 == Виведіть змінну alice_in_wonderland на друк
 
 """
     # Задачі 04 -10:
