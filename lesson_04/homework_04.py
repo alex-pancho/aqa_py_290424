8c0ced9a553ab64da51e7367a4e7cfd03aa14703
Daria Maksymenko <mydearhope9@gmail.com>
2024-05-16 16:58:09 +0200

    Added solutions for the tasks

--- lesson_04/homework_04.py
+++ lesson_04/homework_04.py
@@ -24,47 +24,65 @@ rolling in wealth."""
 """ Дані у строці adwentures_of_tom_sawer розбиті випадковим чином, через помилку.
 треба замінити кінець абзацу на пробіл .replace("\n", " ")"""
 
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace("\n", " ")
+
 # task 02 ==
 """ Замініть .... на пробіл
 """
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace("....", " ")
 
 # task 03 ==
 """ Зробіть так, щоб у тексті було не більше одного пробілу між словами.
 """
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace("   ", " ")
 
 
 # task 04
 """ Виведіть, скількі разів у тексті зустрічається літера "h"
 """
-
+print(adwentures_of_tom_sawer.count("h"))
 
 # task 05
 """ Виведіть, скільки слів у тексті починається з Великої літери?
 """
+uppercase_letters_amount = 0
+for i in adwentures_of_tom_sawer:
+    if i.isupper():
+        uppercase_letters_amount = uppercase_letters_amount + 1
 
+print(f'{uppercase_letters_amount} слів починається з великої літери' )
 
 # task 06
 """ Виведіть позицію, на якій слово Tom зустрічається вдруге
 """
-
+word_tom_position = adwentures_of_tom_sawer.find("Tom")
+print(adwentures_of_tom_sawer.find("Tom", word_tom_position + 1))
 
 # task 07
 """ Розділіть змінну adwentures_of_tom_sawer по кінцю речення.
 Збережіть результат у змінній adwentures_of_tom_sawer_sentences
 """
-adwentures_of_tom_sawer_sentences = None
+adwentures_of_tom_sawer_sentences = adwentures_of_tom_sawer.split(".")
+
+print(adwentures_of_tom_sawer_sentences)
+
 
 # task 08
 """ Виведіть четверте речення з adwentures_of_tom_sawer_sentences.
 Перетворіть рядок у нижній регістр.
 """
 
+print(adwentures_of_tom_sawer_sentences[3].lower())
 
 # task 09
 """ Перевірте чи починається якесь речення з "By the time".
 """
 
+for sentence in adwentures_of_tom_sawer_sentences:
+    if sentence.lstrip().startswith("By the time"):
+        print("There is a sentence starting with By the time")
 
 # task 10
 """ Виведіть кількість слів останнього речення з adwentures_of_tom_sawer_sentences.
 """
+print(adwentures_of_tom_sawer_sentences[4].count(" "))
