2591d6556bf73a7bfbafa62ddefcce5d14a097f9
Maryna Stritenko <stritenkoqa@gmail.com>
2024-05-27 16:29:25 +0300

    tasks 9,10 were added to the HW_6

--- lesson_06/homework_06.py
+++ lesson_06/homework_06.py
@@ -4,12 +4,12 @@
 Напишіть оператор if, щоб перевірити, чи колір прибульця 'green'.
 Якщо так, надрукуйте повідомлення про те, що гравець щойно заробив 5 балів.
 """
+
 # my answer:
 alien_color = 'green'
 if alien_color == 'green':
     print("You won 5 points")
-
-    
+  
 
 # task 2
 """  Скопіюйте пеопередню відповідь, змініть і доповніть її умовою else.
@@ -17,12 +17,15 @@ if alien_color == 'green':
 Якщо колір прибульця не зелений, надрукуйте, що гравець щойно заробив 10 балів.
 Зробіть так, щоб виводилася умова else.
 """
+
 # my answer:
 alien_color = 'green'
 if alien_color == 'red':
     print("You won 5 points")
 else:
     print("You won 10 points")
+
+
 # task 3
 # task 4
 """  Скопіюйте пеопередню відповідь, змініть і доповніть її умовою elif.
@@ -32,6 +35,7 @@ else:
 Якщо прибулець червоний, надрукуйте повідомлення про те, що гравець заробив 15 очок
 + напишіть цикл for що перебере і обробить всі значення списку alien_color
 """
+
 # my answer:
 alien_color = ['green', 'yellow', 'red']
 for i in alien_color:
@@ -41,11 +45,14 @@ for i in alien_color:
         print("You won 15 points")
     else:
         print("You won 10 points")
+
+
 # task 5
 """  Начинки для піци (pizza_topping): напишіть цикл, який пропонує користувачеві ввести ряд начинок
 для піци, доки він не введе значення 'quit'. Коли вони введуть кожну начинку,
 надрукуйте повідомлення про те, що ви додасте цю начинку до їхньої піци.
 """
+
 # my answer:
 while True:
     pizza_topping = input("Enter the topping: ")
@@ -55,6 +62,7 @@ while True:
     else:
         print(f"Your topping {pizza_topping} is added.")
   
+
 # task 6
 """  Напишіть програму, яка знаходить суму всіх цифр цілого числа, яке вводить користувач.
 Для перебору вводу від користувача використовуйте цикл. Виведіть суму цифр числа на екран.
@@ -62,6 +70,7 @@ while True:
 Введіть число: 12345
 Сума цифр числа 12345: 15
 """
+
 # my answer:
 number = input("Enter the number: ")
 sum_all_digits = 0
@@ -71,6 +80,7 @@ for digit in number:
 
 print(f"The sum of the digit of the number {number} is {sum_all_digits} ")
 
+
 # task 7
 """  Потрібно написати програму, яка просить користувача ввести числа, доки він не введе 0.
 Програма повинна підрахувати суму всіх введених чисел, окрім 0, і вивести її на екран.
@@ -78,6 +88,7 @@ print(f"The sum of the digit of the number {number} is {sum_all_digits} ")
 Після введеня 0 показує результат сумування.
 Розв'язати з використанням циклу while та break
 """
+
 # my answer:
 sum = 0
 while True:
@@ -89,6 +100,7 @@ while True:
 
 print("The sum of the numbers is: ",sum)
 
+
 # task 8
 """  З використанням циклу for реалізуйте гру "Вгадай число".
 Початок програми написаний, гравець має 5 спроб відгадати випадкове число від 1 до 20,
@@ -96,8 +108,10 @@ print("The sum of the numbers is: ",sum)
 У кожній спробі гравець вводить своє припущення, після чого програма повідомляє, чи
 було припущення занадто великим або занадто малим, чи гравець вгадав число.
 """
+
+# my answer:
 import random
-secret_number = random.randint(1, 20)
+secret_number = random.randint(1, 5)
 guesses = 0
 max_guesses = 5
 for guesses in  range(max_guesses):
@@ -108,17 +122,28 @@ for guesses in  range(max_guesses):
     elif guess > secret_number:
         print("You guess is too big.")
     else:
-        print(f"You guessed the number in { guess + 1} attempts")
+        print(f"You guessed the number in { guess } attempts")
+
+
 # task 9
 """  Задача з використанням циклу for та continue. Задано список фруктів 'fruits'
 потрібно вивести на екран всі елементи списку, окрім "orange".
 """
+
+# my answer
 fruits = ["apple", "banana", "orange", "grape", "mango"]
+for fruit in fruits:
+    if fruit == 'orange':
+        continue
+    print(fruit, end = ' ')
+
 
 # task 10
 """  Задано список чисел numbers, потрібно знайти список квадратів
 парних чисел зі списку. Спробуйте використати if та цикл for в один рядок.
 """
+
+# my answer:
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-result = ["Відповідь вставте сюди"]
+result = [num ** 2 for num in numbers if num % 2 == 0]
 print(result)  #  [4, 16, 36, 64, 100]
