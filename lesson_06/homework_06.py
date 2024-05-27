219eb9b75767baf9af80c63cd7e6514e14b54fc7
Maryna Stritenko <stritenkoqa@gmail.com>
2024-05-27 16:02:56 +0300

    hw_6 was added

--- lesson_06/homework_06.py
+++ lesson_06/homework_06.py
@@ -4,6 +4,12 @@
 Напишіть оператор if, щоб перевірити, чи колір прибульця 'green'.
 Якщо так, надрукуйте повідомлення про те, що гравець щойно заробив 5 балів.
 """
+# my answer:
+alien_color = 'green'
+if alien_color == 'green':
+    print("You won 5 points")
+
+    
 
 # task 2
 """  Скопіюйте пеопередню відповідь, змініть і доповніть її умовою else.
@@ -11,7 +17,12 @@
 Якщо колір прибульця не зелений, надрукуйте, що гравець щойно заробив 10 балів.
 Зробіть так, щоб виводилася умова else.
 """
-
+# my answer:
+alien_color = 'green'
+if alien_color == 'red':
+    print("You won 5 points")
+else:
+    print("You won 10 points")
 # task 3
 # task 4
 """  Скопіюйте пеопередню відповідь, змініть і доповніть її умовою elif.
@@ -21,13 +32,29 @@
 Якщо прибулець червоний, надрукуйте повідомлення про те, що гравець заробив 15 очок
 + напишіть цикл for що перебере і обробить всі значення списку alien_color
 """
-
+# my answer:
+alien_color = ['green', 'yellow', 'red']
+for i in alien_color:
+    if i == 'green':
+        print("You won 5 points")
+    elif i == 'red':
+        print("You won 15 points")
+    else:
+        print("You won 10 points")
 # task 5
 """  Начинки для піци (pizza_topping): напишіть цикл, який пропонує користувачеві ввести ряд начинок
 для піци, доки він не введе значення 'quit'. Коли вони введуть кожну начинку,
 надрукуйте повідомлення про те, що ви додасте цю начинку до їхньої піци.
 """
-
+# my answer:
+while True:
+    pizza_topping = input("Enter the topping: ")
+    if pizza_topping == 'quit':
+        print("Finished adding toppings.")
+        break
+    else:
+        print(f"Your topping {pizza_topping} is added.")
+  
 # task 6
 """  Напишіть програму, яка знаходить суму всіх цифр цілого числа, яке вводить користувач.
 Для перебору вводу від користувача використовуйте цикл. Виведіть суму цифр числа на екран.
@@ -35,6 +62,14 @@
 Введіть число: 12345
 Сума цифр числа 12345: 15
 """
+# my answer:
+number = input("Enter the number: ")
+sum_all_digits = 0
+
+for digit in number:
+    sum_all_digits += int(digit)
+
+print(f"The sum of the digit of the number {number} is {sum_all_digits} ")
 
 # task 7
 """  Потрібно написати програму, яка просить користувача ввести числа, доки він не введе 0.
@@ -43,6 +78,16 @@
 Після введеня 0 показує результат сумування.
 Розв'язати з використанням циклу while та break
 """
+# my answer:
+sum = 0
+while True:
+    number = int(input("Enter a number: "))
+    if number == 0:
+      break
+    else:
+        sum += number
+
+print("The sum of the numbers is: ",sum)
 
 # task 8
 """  З використанням циклу for реалізуйте гру "Вгадай число".
@@ -55,8 +100,15 @@ import random
 secret_number = random.randint(1, 20)
 guesses = 0
 max_guesses = 5
-print("Вгадайте число від 1 до 20 за 5 спроб!")
+for guesses in  range(max_guesses):
+    guess = int(input("Enter your guess: "))
 
+    if guess < secret_number:
+        print("You guess is too small.")
+    elif guess > secret_number:
+        print("You guess is too big.")
+    else:
+        print(f"You guessed the number in { guess + 1} attempts")
 # task 9
 """  Задача з використанням циклу for та continue. Задано список фруктів 'fruits'
 потрібно вивести на екран всі елементи списку, окрім "orange".
