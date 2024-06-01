3a267ee47563897d3fdd0e1e8219294683e40c3f
Ilya <ilyanechytaylo@gmail.com>
2024-06-01 18:26:20 +0300

    Added HW7

--- lesson_07/homework_07.py
+++ lesson_07/homework_07.py
@@ -8,18 +8,16 @@ def multiplication_table(number):
     multiplier = 1
 
     # Complete the while loop condition.
-    while multiplier <= number:
+    while multiplier:
         result = number * multiplier
-        # десь тут помила, а може не одна
-        if  result > "25":
-            # Enter the action to take if the result is greater than 25
-            pass
+        if result > 25:
+            break
         print(str(number) + "x" + str(multiplier) + "=" + str(result))
 
-        # Increment the appropriate variable
-        multi += 1
+        multiplier += 1
 
-multiplication_table(3)
+multiplication_table(4)
+
 # Should print:
 # 3x1=3
 # 3x2=6
@@ -32,26 +30,53 @@ multiplication_table(3)
 """  Написати функцію, яка обчислює суму двох чисел.
 """
 
+def sum_digits(a,b):
+    """Обчислює суму двох чисел"""
+    sum_of_digits = a + b
+    print(f'Сума двох чисел дорівнює -> {sum_of_digits}')
+
+sum_digits(10, 12)
 
 # task 3
 """  Написати функцію, яка розрахує середнє арифметичне списку чисел.
 """
+def average(*args):
+    """Обчислює середнє арифмитичне списку чисел"""
+    average_digits = sum(args)/len(args)
+    print(f'Середнє арифмитичне списку чисел -> {average_digits}')
 
+average(10,20,30)
 # task 4
 """  Написати функцію, яка приймає рядок та повертає його у зворотному порядку.
 """
+def reverse_string(input_string):
+    """Приймає рядок та повертає його у зворотньому порядку"""
+    return input_string[::-1]
+
+print(reverse_string('Hello, world!'))
 
 # task 5
 """  Написати функцію, яка приймає список слів та повертає найдовше слово у списку.
 """
+def longest_word(word_list):
+    if not word_list:
+        return None
 
+    longest = word_list[0]
+
+    for word in word_list:
+        if len(word) > len(longest):
+            longest = word
+
+    return longest
+
+print(longest_word(['test1', 'test2', 'test333', 'test55555']))
 # task 6
 """  Написати функцію, яка приймає два рядки та повертає індекс першого входження другого рядка
 у перший рядок, якщо другий рядок є підрядком першого рядка, та -1, якщо другий рядок
 не є підрядком першого рядка."""
 def find_substring(str1, str2):
-
-    return -1
+    return str1.find(str2)
 
 str1 = "Hello, world!"
 str2 = "world"
@@ -62,10 +87,41 @@ str2 = "cat"
 print(find_substring(str1, str2)) # поверне -1
 
 # task 7
+def square_number_in_list(*args):
+    """Дана функція підносить всі числа у списку до другого степеня"""
+    result = [num * num for num in args]
+    return result
+print(square_number_in_list(1,2,3,4,8))
+
 # task 8
+def unique_elements_in_list(list1):
+    """Повертає виключно унікальні значення з заданого списку"""
+    unique = list(set(list1))
+    return unique
+
+print(unique_elements_in_list([1,2,3,4,5,5,6,6,7,7,8,8,8]))
+
 # task 9
+# Оскільки домашек трохи менше робив в цій групі, вирішив використати домашки з минулої групи в якості написання функцій :) Сподіваюсь, це ок
+def is_palindrome(string):
+    """Перевіряє, чи є стрінга паліндромом"""
+    return string == string[::-1]
+
+string = 'level'
+if is_palindrome(string):
+    print(f'Рядок "{string}" є паліндромом')
+else:
+    print(f'Рядок "{string}" не є паліндромом')
+
 # task 10
-"""  Оберіть будь-які 4 таски з попередніх домашніх робіт та
-перетворіть їх у 4 функції, що отримують значення та повертають результат.
-Обоязково документуйте функції та дайте зрозумілі імена змінним.
-"""
+
+def is_even(number):
+    """Перевіряє, чи є число парним"""
+    statement = number % 2 == 0
+    return statement
+
+num = 11
+if is_even(num):
+    print(f"Число {num} є парним.")
+else:
+    print(f"Число {num} не є парним.")
