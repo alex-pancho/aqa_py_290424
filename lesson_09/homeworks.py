b7f61a91f2dd6e62026f1ec584bbb30d270e48a8
Daria Maksymenko <mydearhope9@gmail.com>
2024-06-01 18:55:03 +0200

    Homework_09: Covered 4 functions with unittests

--- lesson_09/homeworks.py
+++ lesson_09/homeworks.py
@@ -1,2 +1,40 @@
 """ Сюди підготовані ф-ції вставити
 """
+
+def calc_words_uppercase(string_to_count_in):
+    """Calculates the amount of words starting with the uppercase letter."""
+    uppercase_letters_amount = 0
+    for i in string_to_count_in:
+        if i.isupper():
+            uppercase_letters_amount = uppercase_letters_amount + 1
+
+    return uppercase_letters_amount
+
+def longest_word(*args):
+    max_length = 0
+    max_length_word = ""
+    for arg in args:
+        word_length = len(arg)
+        if word_length > max_length:
+            max_length = word_length
+            max_length_word = arg
+
+    return max_length_word
+
+def find_substring(str1, str2):
+    for i in range(0, len(str1)):
+        for j in range(0, len(str2)):
+            if i + j > len(str1) or str1[i + j] != str2[j]:
+                break
+            elif j == len(str2) - 1:
+                return i
+
+    return -1
+
+def has_duplicates(list_to_check):
+    """Returns True if list has duplicates."""
+    if len(list_to_check) == 0:
+        return False
+
+    list_to_check_set = set(list_to_check)
+    return len(list_to_check) != len(list_to_check_set)
--- lesson_09/test_homeworks09.py
+++ lesson_09/test_homeworks09.py
@@ -7,5 +7,88 @@
 На оцінку впливає як якість тестів так і розмір тестового покриття.
 Мінімум на 10 балів - 1 правильно задизайнений позитивний тест на функцію.
 """
+import unittest
 import homeworks
 
+class MyTest(unittest.TestCase):
+
+    def test_01_calc_words_uppercase(self):
+        test_string = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. \
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \
+        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+        actual_result = homeworks.calc_words_uppercase(test_string)
+        expected_result = 5
+        self.assertEqual(actual_result, expected_result, f"The expected amount of words starting with uppercase is {expected_result}")
+
+    def test_02_calc_words_uppercase(self):
+        test_string = ""
+        actual_result = homeworks.calc_words_uppercase(test_string)
+        expected_result = 0
+        self.assertEqual(actual_result, expected_result, f"The expected amount of words starting with uppercase is {expected_result}")
+
+# And here I actually found out that my function is not working correctly :)
+    def test_03_clac_words_uppercase(self):
+        test_string = "LOrem IpSum is simply dummy text of the printing and typesetting industry."
+        actual_result = homeworks.calc_words_uppercase(test_string)
+        expected_result = 2
+        self.assertEqual(actual_result, expected_result, f"The expected amount of words starting with uppercase is {expected_result}")
+
+    def test_01_longest_word(self):
+        actual_result = homeworks.longest_word("table", "computer", "wall", "cup")
+        expected_result = "computer"
+        self.assertEqual(actual_result, expected_result, f"The expected longest word is {expected_result}")
+
+    def test_02_longest_word(self):
+        actual_result = homeworks.longest_word("table", "computer", "wall", "cup", "approach")
+        expected_result = "computer"
+        self.assertEqual(actual_result, expected_result, f"The expected longest word is {expected_result}")
+
+    def test_03_longest_word(self):
+        with self.assertRaises(TypeError):
+            homeworks.longest_word(1, 2, 3, 4, 5)
+
+    def test_01_find_substring(self):
+        str1 = "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+        str2 = "has survived"
+        actual_result = homeworks.find_substring(str1, str2)
+        expected_result = 3
+        self.assertEqual(actual_result, expected_result, f"The expected position of the substring is {expected_result}")
+
+    def test_02_find_substring(self):
+        str1 = "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+        str2 = "is done"
+        actual_result = homeworks.find_substring(str1, str2)
+        expected_result = -1
+        self.assertEqual(actual_result, expected_result, f"The expected position of the substring is {expected_result}")
+
+    def test_03_find_substring(self):
+        str1 = "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+        str2 = ""
+        actual_result = homeworks.find_substring(str1, str2)
+        expected_result = -1
+        self.assertEqual(actual_result, expected_result, f"The expected position of the substring is {expected_result}")
+
+    def test_04_find_substring(self):
+        str1 = ""
+        str2 = "has survived"
+        actual_result = homeworks.find_substring(str1, str2)
+        expected_result = -1
+        self.assertEqual(actual_result, expected_result, f"The expected position of the substring is {expected_result}")
+
+    def test_01_has_duplicates(self):
+        test_list = [1, 4, 4, 5, 8, 0]
+        actual_result = homeworks.has_duplicates(test_list)
+        self.assertTrue(actual_result, f"List doesn't have duplicates")
+
+    def test_02_has_duplicates(self):
+        test_list = [1, 3, 4, 5, 8, 0]
+        actual_result = homeworks.has_duplicates(test_list)
+        self.assertFalse(actual_result, "List has duplicates")
+
+    def test_03_has_duplicates(self):
+        test_list = []
+        actual_result = homeworks.has_duplicates(test_list)
+        self.assertFalse(actual_result, "List has duplicates")
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
