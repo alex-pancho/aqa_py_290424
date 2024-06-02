2190c673da5168e1a45d909bea3fa0db614e2951
Serhii Kotsar <sergejkotsar94@gmail.com>
2024-06-02 12:00:55 +0300

    hw 9 was added, unittests

--- lesson_09/homeworks.py
+++ lesson_09/homeworks.py
@@ -1,2 +1,30 @@
-""" Сюди підготовані ф-ції вставити
-"""
+# Сюди підготовані ф-ції вставити
+
+
+def calculator(expression):
+    allowed = '+-/*'
+    if not any(sign in expression for sign in allowed):
+        raise ValueError(f'Выражение должно содержать хотя бы один знак ({allowed})')
+    for sign in allowed:
+        if sign in expression:
+            try:
+                left, right = expression.split(sign)
+                left, right = int(left), int(right)
+            except (ValueError, TypeError):
+                raise ValueError('Выражение должно содержать 2 целых числа и 1 знак')
+
+            if sign == '+':
+                return left + right
+            elif sign == '-':
+                return left - right
+            elif sign == '*':
+                return left * right
+            elif sign == '/':
+                try:
+                    return left / right
+                except ZeroDivisionError:
+                    raise ValueError('Запрещено делить на 0')
+
+
+if __name__ == '__main__':
+    print(calculator('10+2+3'))
--- lesson_09/test_homeworks09.py
+++ lesson_09/test_homeworks09.py
@@ -7,5 +7,53 @@
 На оцінку впливає як якість тестів так і розмір тестового покриття.
 Мінімум на 10 балів - 1 правильно задизайнений позитивний тест на функцію.
 """
-import homeworks
+from lesson_09.homeworks import calculator
+from unittest import TestCase, main
 
+
+class CalculatorTest(TestCase):
+    def test_plus(self):
+        self.assertEqual(calculator('2+2'), 4)
+
+    def test_minus(self):
+        self.assertEqual(calculator('3-1'), 2)
+
+    def test_multi(self):
+        self.assertEqual(calculator('4*4'), 16)
+
+    def test_divide(self):
+        self.assertEqual(calculator('10/5'), 2.0)
+
+    def test_no_signs(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('abracadabra')
+        self.assertEqual('Выражение должно содержать хотя бы один знак (+-/*)', e.exception.args[0])
+
+    def test_two_equal_signs(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('2+2+2')
+        self.assertEqual('Выражение должно содержать 2 целых числа и 1 знак', e.exception.args[0])
+
+    def test_many_signs(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('2+3*10')
+        self.assertEqual('Выражение должно содержать 2 целых числа и 1 знак', e.exception.args[0])
+
+    def test_no_ints(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('2.2+3.0')
+        self.assertEqual('Выражение должно содержать 2 целых числа и 1 знак', e.exception.args[0])
+
+    def test_strings(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('a+b')
+        self.assertEqual('Выражение должно содержать 2 целых числа и 1 знак', e.exception.args[0])
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('10/0')
+        self.assertEqual('Запрещено делить на 0', e.exception.args[0])
+
+
+if __name__ == '__main__':
+    main()
