d9f08a885ce389897ba41c4bab9c6559e36e0edf
kit <sophia.lavrova@gmail.com>
2024-06-06 12:01:53 +0300

    added functions to be covered with unit tests, added unit tests for functions

--- lesson_09/homeworks.py
+++ lesson_09/homeworks.py
@@ -1,2 +1,36 @@
 """ Сюди підготовані ф-ції вставити
 """
+# function 1
+def square(black_sea_square, azov_sea_square):
+    return black_sea_square + azov_sea_square
+black_sea_square = 436_402
+azov_sea_square = 37_800
+total_square = square(black_sea_square, azov_sea_square)
+print(f"The Black Sea and the Sea of Azov together cover {total_square} km2")
+
+
+# function 2
+def computer_price(monthly_price, number_of_months):
+    """counting total computer price based on monthly payment and the number of months"""
+    return monthly_price * number_of_months
+monthly_price = 1179
+number_of_months = 18
+total_price = computer_price(monthly_price, number_of_months)
+print(f"The total computer price is {total_price} UAH")
+
+# function 3
+def diff_between_two_sets(set_1, set_2):
+    return(set_1.symmetric_difference(set_2))
+set_1 = {1, 2, 3, 4, 5}
+set_2 = {4, 6, 5, 10}
+diff = diff_between_two_sets(set_1, set_2)
+formatted_diff = ', '.join(map(str, diff))
+print(f"Unique numbers between 2 sets: {formatted_diff}.")
+
+# function 4
+def unique_values_from_list(small_list):
+    return (set(small_list))
+small_list = [3, 1, 4, 5, 2, 5, 3]
+unique_values_output = unique_values_from_list(small_list)
+formatted_unique_values_output = ', '.join(map(str, unique_values_output))
+print(f"Unique values from the small_list are: {formatted_unique_values_output}.")
--- lesson_09/test_homeworks09.py
+++ lesson_09/test_homeworks09.py
@@ -7,5 +7,40 @@
 На оцінку впливає як якість тестів так і розмір тестового покриття.
 Мінімум на 10 балів - 1 правильно задизайнений позитивний тест на функцію.
 """
+
+import unittest
 import homeworks
+
+class TestFunctions(unittest.TestCase):
+
+    def test_square(self):
+        black_sea_square = 436_402
+        azov_sea_square = 37_800
+        actual_result = homeworks.square(black_sea_square, azov_sea_square)
+        expected_result = 474_202
+        self.assertEqual(actual_result, expected_result, f"The sum of {black_sea_square} and {azov_sea_square} should be {expected_result}")
+
+    def test_computer_price(self):
+        monthly_price = 1179
+        number_of_months = 18
+        actual_result = homeworks.computer_price(monthly_price, number_of_months)
+        expected_result = 21222
+        self.assertEqual(actual_result, expected_result, f"The total computer price should be {expected_result}")
+
+    def test_diff_between_two_sets(self):
+        set_1 = {1, 2, 3, 4, 5}
+        set_2 = {4, 6, 5, 10}
+        actual_result = homeworks.diff_between_two_sets(set_1, set_2)
+        expected_result = {1, 2, 3, 6, 10}
+        self.assertEqual(actual_result, expected_result, f"The symmetric difference between {set_1} and {set_2} should be {expected_result}")
+
+    def test_unique_values_from_list(self):
+        small_list = [3, 1, 4, 5, 2, 5, 3]
+        actual_result = homeworks.unique_values_from_list(small_list)
+        expected_result = {1, 2, 3, 4, 5}
+        self.assertEqual(actual_result, expected_result, f"The unique values from the list {small_list} should be {expected_result}")
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
+
 
