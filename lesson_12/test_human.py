1d3ab84e9f781798b988d5114f39ae9d52a771f1
Serhii Kotsar <sergejkotsar94@gmail.com>
2024-06-22 12:18:23 +0300

    hw 11 was done

--- lesson_12/test_human.py
+++ lesson_12/test_human.py
@@ -1,5 +1,5 @@
 import pytest
-from lesson_12.homework_12 import Human
+from homework_12 import Human
 
 
 # Фикстура, создаёт и возвращает экземпляр класса Human с начальными параметрами для всех тестов
@@ -12,6 +12,30 @@ def test_to_eat(setup_person):
     assert setup_person.energy == 100
     setup_person.to_eat()
     assert setup_person.energy == 105
+
+
+def test_to_sleep(setup_person):
+    assert setup_person.energy == 100
+    setup_person.to_sleep()
+    assert setup_person.energy == 110
+
+
+def test_to_talk(setup_person):
+    assert setup_person.energy == 100
+    setup_person.to_talk()
+    assert setup_person.energy == 95
+
+
+def test_to_walk(setup_person):
+    assert setup_person.energy == 100
+    setup_person.to_walk()
+    assert setup_person.energy == 90
+
+
+def test_do_homework(setup_person):
+    assert setup_person.energy == 100
+    setup_person.to_do_homework()
+    assert setup_person.energy == 10
 
 
 if __name__ == '__main__':
