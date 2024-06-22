2ccaa279660cec3819974cfd184f12cb1df95f01
Serhii Kotsar <sergejkotsar94@gmail.com>
2024-06-22 12:32:25 +0300

    test were added

--- lesson_12/test_human.py
+++ lesson_12/test_human.py
@@ -8,6 +8,41 @@ def setup_person():
     return Human(first_name='Alex', second_name='Shevchenko', birthday='10.07.1986', sex='male')
 
 
+def test_human_initialization():
+    human = Human(first_name='Oleg', second_name='Panasenko', birthday='06.11.1989', sex='male')
+    assert human.first_name == 'Oleg'
+    assert human.second_name == 'Panasenko'
+    assert human.birthday == '06.11.1989'
+    assert human.sex == 'male'
+    assert human.energy == 100
+
+
+def test_human_attribute_name_change(setup_person):
+    assert setup_person.first_name == 'Alex'
+    setup_person.first_name = 'Serhii'
+    assert setup_person.first_name == 'Serhii'
+
+
+def test_human_attribute_second_name_change(setup_person):
+    assert setup_person.second_name == 'Shevchenko'
+    setup_person.second_name = 'Makarenko'
+    assert setup_person.second_name == 'Makarenko'
+
+
+def test_human_attribute_sex_change(setup_person):
+    assert setup_person.sex == 'male'
+    setup_person.sex = 'female'
+    assert setup_person.sex == 'female'
+
+
+def test_human_attribute_types(setup_person):
+    assert isinstance(setup_person.first_name, str)
+    assert isinstance(setup_person.second_name, str)
+    assert isinstance(setup_person.birthday, str)
+    assert isinstance(setup_person.sex, str)
+    assert isinstance(setup_person.energy, int)
+
+
 def test_to_eat(setup_person):
     assert setup_person.energy == 100
     setup_person.to_eat()
