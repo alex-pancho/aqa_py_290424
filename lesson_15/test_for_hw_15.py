f41effdb03ba4f93aade7c2c442c83b928c8ff0d
Tesliuk Vlad <vvv3410@gmail.com>
2024-06-25 22:27:36 +0300

    done 15

--- lesson_15/test_for_hw_15.py
+++ lesson_15/test_for_hw_15.py
@@ -20,5 +20,24 @@ class TestLoginSystem(unittest.TestCase):
             log_content = log_file.read()
             self.assertIn('Login event - Username: john_doe, Status: success', log_content)
 
+    def test_expired_password(self):
+        log_event('john_doe', 'expired')
+        with open('login_system.log', 'r') as log_file:
+            log_content = log_file.read()
+            self.assertIn('Login event - Username: john_doe, Status: expired', log_content)
+
+    def test_failed_login(self):
+        log_event('john_doe', 'failed')
+        with open('login_system.log', 'r') as log_file:
+            log_content = log_file.read()
+            self.assertIn('Login event - Username: john_doe, Status: failed', log_content)
+
+    def test_unknown_status(self):
+        log_event('john_doe', 'unknown')
+        with open('login_system.log', 'r') as log_file:
+            log_content = log_file.read()
+            self.assertIn('Login event - Username: john_doe, Status: unknown', log_content)
+
+
 if __name__ == "__main__":
     unittest.main(verbosity=2)
