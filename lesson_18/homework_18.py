93444d8a05cdb9bba23d4d7c281d07335bbfdd13
Tesliuk Vlad <vvv3410@gmail.com>
2024-07-07 19:14:36 +0300

    done 18

---
+++ lesson_18/homework_18.py
@@ -0,0 +1,23 @@
+
+
+def log_factorial_calls(func):
+    def wrapper(n):
+        print(f"Calling {func.__name__}({n})")
+        result = func(n)
+        print(f"Result: {result}")
+        return result
+    return wrapper
+
+@log_factorial_calls
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+def factorial_generator(n):
+    for i in range(n + 1):
+        yield factorial(i)
+
+for fact in factorial_generator(5):
+    pass
