846688285384de83fcb49e4235f6534ad117c0ae
Ilya <ilyanechytaylo@gmail.com>
2024-07-07 18:22:39 +0300

    Added hw13

--- lesson_15/test_for_hw_15.py
+++ lesson_15/test_for_hw_15.py
@@ -9,7 +9,7 @@ from homework_15 import log_event
 
 class TestLoginSystem(unittest.TestCase):
 
-    def setUp(self):
+    def setUp(self) -> None:
         # Очищення лог-файлу перед кожним тестом
         with open('login_system.log', 'w'):
             pass
@@ -19,6 +19,26 @@ class TestLoginSystem(unittest.TestCase):
         with open('login_system.log', 'r') as log_file:
             log_content = log_file.read()
             self.assertIn('Login event - Username: john_doe, Status: success', log_content)
+
+    def test_failed_login(self):
+        log_event('jane_smith', 'failure')
+        with open('login_system.log', 'r') as log_file:
+            log_content = log_file.read()
+            self.assertIn('Login event - Username: jane_smith, Status: failure', log_content)
+
+    def test_multiple_users(self):
+        log_event('john_doe', 'success')
+        log_event('jane_smith', 'success')
+        with open('login_system.log', 'r') as log_file:
+            log_content = log_file.read()
+            self.assertIn('Login event - Username: john_doe, Status: success', log_content)
+            self.assertIn('Login event - Username: jane_smith, Status: success', log_content)
+
+    def test_expired_login(self):
+        log_event('jane_smith', 'expired')
+        with open('login_system.log', 'r') as log_file:
+            log_content = log_file.read()
+            self.assertIn('Login event - Username: jane_smith, Status: expired', log_content)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
